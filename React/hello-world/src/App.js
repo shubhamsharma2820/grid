2533e676178421d7a5602a6f3164926f22170390
shubhamsharma2820 <shubham.20apr@gmail.com>
2022-11-23 16:48:13 +0530

    react

--- React/hello-world/src/App.js
+++ React/hello-world/src/App.js
@@ -3,22 +3,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello world
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div className="blank">hello</div>
   );
 }
 
