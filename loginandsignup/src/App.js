0a1c858dfa7d6dbecd67f559bb54809fc16c95bb
shubhamsharma2820 <shubham.20apr@gmail.com>
2023-01-02 22:40:33 +0530

    nill

--- loginandsignup/src/App.js
+++ loginandsignup/src/App.js
@@ -1,8 +1,10 @@
-import logo from './logo.svg';
+import Home from './pages/Home';
 import './App.css';
 
 function App() {
-  
+   return <div>
+    <Home />
+   </div>;
 }
 
 export default App;
---
+++ loginandsignup/src/pages/Home.js
@@ -0,0 +1,9 @@
+ import React from 'react';
+
+class Home extends React.Component {
+    render(){
+        return <h3>hello</h3>;
+          
+    }
+}
+export default Home; 
